4ac64b8772f0efa742816dbde95bdc9f1bf9f184
Kalana <Kalana@email>
2024-11-06 01:51:15 +0530

    buttons

--- HandWrittenDigitRecognition.py
+++ HandWrittenDigitRecognition.py
@@ -1,4 +1,12 @@
+# model
+
 from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+import joblib
+
 
 data_set = load_digits()
 
@@ -8,4 +16,22 @@ target =  data_set.target
 
 print("data shape : ", data.shape)
 print("Images shape : ", images.shape)
-print("data shape : ",target.shape )
+print("target shape : ",target.shape )
+
+plt.imshow(images[0], cmap = 'gray')
+# plt.show()
+
+print(images[0])
+
+train_data, test_data, train_target, test_target = train_test_split(data, target, test_size=0.2)
+
+model = KNeighborsClassifier(n_neighbors=3)
+
+model.fit(train_data, train_target)
+
+predicted_target = model.predict(test_data)
+
+accuracy = accuracy_score(test_target, predicted_target)
+print("Acuracy : ", accuracy)
+
+joblib.dump(model, "KNN-digit_recongnition.sav")
